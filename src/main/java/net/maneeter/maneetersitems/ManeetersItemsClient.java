4a2955451f4d8b0f06416966c3dd1dd0347bbd7d
TheManeeter <maneet.singh863@outlook.com>
2023-07-11 15:34:50 -0400

    Initial commit

---
+++ src/main/java/net/maneeter/maneetersitems/ManeetersItemsClient.java
@@ -0,0 +1,10 @@
+package net.maneeter.maneetersitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ManeetersItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/maneeter/maneetersitems/ManeetersItemsGenerator.java
@@ -0,0 +1,11 @@
+package net.maneeter.maneetersitems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ManeetersItemsGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
