da3a3a1e24d19ff967b39701e5092f6eec41fd18
TheManeeter <maneet.singh863@outlook.com>
2023-07-12 11:36:27 -0400

    Add Luranium

--- src/main/java/net/maneeter/maneetersitems/ManeetersItems.java
+++ src/main/java/net/maneeter/maneetersitems/ManeetersItems.java
@@ -2,15 +2,17 @@ package net.maneeter.maneetersitems;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.maneeter.maneetersitems.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ManeetersItems implements ModInitializer {
-	public static final String MOD_ID = "luckyitems";
+	public static final String MOD_ID = "maneetersitems";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
